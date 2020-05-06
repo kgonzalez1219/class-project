6cdc18cbdf76bc5e8027d8fdb2fdc51837a14523
Kat <osbornek99@gmail.com>
2020-05-05 21:57:45 -0700

    loop

--- app.js
+++ app.js
@@ -21,16 +21,27 @@ function askName() {
 }
 
 function getProduct() {
-    var response = prompt('Yes or No?');
+    var response = prompt('Would you like to see something really cool or something relaxing?');
     var item;
 
-    if (response === 'Yes') {
-        item = '<img src ="1.jpg">';
-    } else if (response === 'No') {
-        item = '<img src ="2.jpg">';
+    if (response === 'Cool') {
+        item = '<img src ="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/pd36-1-gsfc_20171208_archive_e000012.jpg?w=400&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=c158c378ea1fabc8372250826c2a1c9d">';
+    } else if (response === 'Relaxing') {
+        item = '<img src ="https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/pd19-3-2239751a_1.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=431cd10f133356985c7d6ab3db5fa300">';
     } else {
         item = 'Have a lovely day!';
     }
 
     return document.write('<p>' + item + '</p>');
 }
+
+
+var name = prompt('what\'s your name?');
+var attemptsAllowed = 2; 
+var attempted = 0;
+
+while (name === '' && attempted !== attemptsAllowed) {
+    name = prompt('would you please tell me your name!?');
+    attempted++;
+}
+
